28dcd557f3c5918fdc659bd87e5e79195b45a03d
jamiro.it <jamiro.it@eb1384a5-bd4e-0410-8315-c19b71e326df>
2008-08-27 07:57:51 +0000

    antonio per favore non usare lettere accentate... commentato il codice di laura che non compila

--- trunk/PDCMVoice/src/pdcmvoice/recovery/RecoveryServerThread.java
+++ trunk/PDCMVoice/src/pdcmvoice/recovery/RecoveryServerThread.java
@@ -8,7 +8,7 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 /**
- * 
+ *
  * @author Antonio
  */
 public class RecoveryServerThread extends Thread
@@ -18,18 +18,18 @@ public class RecoveryServerThread extends Thread
     private boolean lastQueryDone;
     private byte[] lastQueryByte;
     private VoiceSession voiceSession;
-    public boolean stop; 
-    
+    public boolean stop;
+
     public RecoveryServerThread(RecoveryConnection RecConn, VoiceSession voiceSession)
 	{
 		this.RecConn = RecConn;
 		lastQuery = null;
 		lastQueryByte = null;
 		lastQueryDone = false;
-		stop = false; 
+		stop = false;
 		this.voiceSession = voiceSession;
 	}
-	
+
 	public void run()
 	{
 	    if (voiceSession != null && RecConn.getRemoteCollection().getPktSize() <= 0)
@@ -39,7 +39,7 @@ public class RecoveryServerThread extends Thread
     	    {
     	        if (RecConn.debug)
                     System.out.println("--SERVER-- Acquisizione dimensione pacchetto codificato");
-    	        
+
     	        try {
                     Thread.sleep(500); //attesa durante l'acquisizione della dimensione
                 } catch (InterruptedException e1) {
@@ -47,16 +47,16 @@ public class RecoveryServerThread extends Thread
                     e1.printStackTrace();
                 }
     	    }
-    	    
+
     	    this.RecConn.getLocalCollection().setPktSize(voiceSession.lastEncodedFrameSize());
     	    if (RecConn.debug)
                 System.out.println("--SERVER-- Dimensione pacchetto codificato: " + voiceSession.lastEncodedFrameSize());
         }
-	    
+
 	  //##APERTURA DEGLI STREAM SUL CLIENTSOCKET
 	    DataInputStream dis = null;
 	    DataOutputStream dos = null;
-	    
+
 	    try {
             //DataInputStream dis = new DataInputStream(RecConn.getServerSocket().getInputStream());
 		    dis = new DataInputStream(RecConn.getClientSocket().getInputStream());
@@ -65,20 +65,20 @@ public class RecoveryServerThread extends Thread
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        
-        
+
+
 //        try {
 //            Thread.sleep(500); //attesa prima di partire
 //        } catch (InterruptedException e1) {
 //            // TODO Auto-generated catch block
 //            e1.printStackTrace();
 //        }
-        
-        
-        boolean endServerThread = false; 
-        
+
+
+        boolean endServerThread = false;
+
         while (!endServerThread)
-        {    
+        {
             int l = 0; //attesa input
             boolean queryRead = false;
             boolean lengthRead = false;
@@ -88,7 +88,7 @@ public class RecoveryServerThread extends Thread
             int f = 0; //lettura lunghezza
             lastQueryByte = null;
             int k = 0; // indice lastQueryByte
-            
+
           //##ANALISI DELLO STREAM IN INPUT: VERIFICA FINE RICHIESTE DA PARTE DEL CLIENT REMOTO E ANALISI LUNGHEZZA E CONTENUTO DELLA QUERY
             while (!endServerThread && (!lengthRead || !queryRead))
             {
@@ -98,8 +98,8 @@ public class RecoveryServerThread extends Thread
                     // TODO Auto-generated catch block
                     e1.printStackTrace();
                 }
-                
-              //##LETTURA LUNGHEZZA QUERY E VERIFICA DELL'EVENTUALE COMUNICAZIONE "END OF QUERY" 
+
+              //##LETTURA LUNGHEZZA QUERY E VERIFICA DELL'EVENTUALE COMUNICAZIONE "END OF QUERY"
                 if (!lengthRead)
                 {
                     try {
@@ -107,10 +107,10 @@ public class RecoveryServerThread extends Thread
                         {
                         if (RecConn.debug)
                             System.out.println("attesa lunghezza query..." + l++);
-                        
+
     //                        if (RecConn.debug)
     //                            System.out.println("br.ready...");
-    //                        
+    //
                             if (f == 0)
                                 queryLength1 = dis.readByte();
                             else if (f == 1)
@@ -120,7 +120,7 @@ public class RecoveryServerThread extends Thread
                             f++;
                             if (f >= 2)
                             {
-                                
+
                               //##CONTROLLO COMUNICAZIONE "FINE DELLE RICHIESTE" DA PARTE DEL CLIENT
                                 queryLength = RecoveryCollection.mergeBytes(queryLength1, queryLength2);
                                 //if (queryLength == 0 && dis.readByte() == 2) //salto il byte di controllo
@@ -143,7 +143,7 @@ public class RecoveryServerThread extends Thread
                                     lengthRead = true;
                                     dis.readByte();//salto il byte di controllo
                                     lastQueryByte = new byte[queryLength];
-                                    
+
                                     //lunghezza della query vera e propria (esclusi i 3 byte che indicano la lunghezza stessa)
                                     if (RecConn.getLocalCollection().debug)
                                         System.out.println("--SERVER-- queryLength: " + queryLength);
@@ -154,15 +154,15 @@ public class RecoveryServerThread extends Thread
                         {
                             System.out.println("--SERVER-- DATA INPUT STREAM (lettura length) ASSENTE");
                         }
-                     
+
                     } catch (IOException e) {
-                        
+
                         if (RecConn.debug)
                             e.printStackTrace();
                         System.out.println("ERROR:queryLength: " + queryLength);
                     }
                 }// end if (!lengthRead)
-                
+
               //##RICEZIONE DELLA QUERY
                 else if (!queryRead)
                 {
@@ -173,12 +173,12 @@ public class RecoveryServerThread extends Thread
                         {
     //                        if (RecConn.debug)
     //                            System.out.println("br.ready...");
-    //                        
+    //
                             lastQueryByte[k++]= dis.readByte();
-                            
+
 //                            if (RecConn.debug)
 //                                System.out.println(k + " - esimo byte della richiesta letto ");
-                            
+
                             if (k >= queryLength)
                             {
                                 queryRead = true;
@@ -190,25 +190,25 @@ public class RecoveryServerThread extends Thread
                             System.out.println("--SERVER-- DATA INPUT STREAM (lettura query) ASSENTE");
                         }
                     } catch (IOException e) {
-                        
+
                         if (RecConn.debug)
                             e.printStackTrace();
                     }
                 }// end if (!queryRead)
-            
-                
+
+
             }
-            
+
           //##DEBUG
             if (this.lastQueryByte != null)
             {
                 if (RecConn.getLocalCollection().debug)
                 {
                     System.out.print("---RICEZIONE QUERY:");
-                    
+
                     for (int i = 0; i<= lastQueryByte.length -1; i++)
                         System.out.print(" " + lastQueryByte[i]);
-                    
+
                     System.out.println("");
                 }
             }
@@ -217,7 +217,7 @@ public class RecoveryServerThread extends Thread
                 {
                     System.out.println("-------ERRORE RICEZIONE QUERY-------");
                 }
-            
+
           //##ELABORAZIONE DELLA QUERY
             if (!endServerThread);
             {
@@ -225,32 +225,32 @@ public class RecoveryServerThread extends Thread
                 int sn  = 0;
                 byte separatore = 0;
                 int start, end;
-                
+
                 int totalePkt = 1;
                 int totaleByteRisposta = 0;
                 byte separatoreMarked = 3;
                 byte separatoreNonMarked = 0;
-                
+
                 int pktSize = RecConn.getLocalCollection().getPktSize(); //dimensione PACCHETTO SINGOLO e non doppio
-                byte[] temp;//contiene il pacchetto associato ad ogni SN; può essere lungo pktSize o 2*pktSize
+                byte[] temp;//contiene il pacchetto associato ad ogni SN; puÃ² essere lungo pktSize o 2*pktSize
                 int lunghezzaTemp = pktSize;
                 byte[] send = new byte[(RecConn.getLocalCollection().getWindowWidth())*pktSize*2];//contiene al massimo windowWidth pacchetti doppi (da ritrasmettere)
-                
+
                 int firstSnOfTheQuery = 0;//significato locale alla query
-                
+
               //##ELABORAZIONE DELLA QUERY RICEVUTA
                 for (int j = 0; j<=queryLength -1; j++) //diverso dal for del RecoveryClientThread
                 {
                   //##LETTURA DEL VALORE SUCCESSIVO
                     sn = RecoveryCollection.mergeBytes(lastQueryByte[j], lastQueryByte[++j]);
                     separatore = lastQueryByte[++j];
-                    
+
                   //##DETERMINAZIONE DEL PRIMO SN DELLA QUERY A CUI TUTTI I SUCCESSIVI VALORI DI OFFSET DELLA QUERY STESSA FARANNO RIFERIMENTO
                     if (j == 2)
                         firstSnOfTheQuery = sn;
                     else
                         sn += firstSnOfTheQuery; //considero vecchio sn come incremento
-                    
+
                   //##DETERMINAZIONE DEGLI INDICI START ED END
                     start = sn;
                     if (separatore == 0)
@@ -259,21 +259,21 @@ public class RecoveryServerThread extends Thread
                     {
                         end = firstSnOfTheQuery + RecoveryCollection.mergeBytes(lastQueryByte[++j], lastQueryByte[++j]);
                         j++; //salto il separatore successivo
-                        
+
                         if (RecConn.getLocalCollection().debug)
                             System.out.println("--SERVER-- end: " + end);
                     }
                     else
                         throw new IllegalArgumentException("posizione " + j);
-                    
+
                   //##RECUPERO PACCHETTI TRA GLI INDICI START ED END (AL LIMITE START = END ==> PACCHETTO ISOLATO)
                     for (int i = start; i <= end; i++)
                     {
                         if (totaleByteRisposta + 2*pktSize +1 -1 >= send.length -1)// il +1 server per il separatoreMarked o NonMarked
                             send = arrayResize(send, 2*send.length);
-                        
+
                         //temp = RecConn.getLocalCollection().read(i);
-                        
+
                         //##LEGGI IL PACCHETTO DALLA COLLEZIONE
                         int n = 1;
                         while (i >= 0 && n <= 5 && (RecConn.getLocalCollection().read(i)== null || RecConn.getLocalCollection().read(i).length == 0))
@@ -284,19 +284,19 @@ public class RecoveryServerThread extends Thread
                                   // TODO Auto-generated catch block
                                   e1.printStackTrace();
                               }
-                                
+
                             if (RecConn.getLocalCollection().debug)
                                 System.out.println("--SERVER-- Attesa lettura pacchetto " + i);
-                            
+
                             n++;
                         }
-                        
+
                         n = i;
-                        
+
                         //##SE LA LETTURA NON E' ANTATA BUON FINE, LEGGI IL PRIMO PACCHETTO VALIDO SUBITO PRECEDENTE A QUELLO RICHIESTO (i)
                         if (n < 0)
                             n = RecConn.getLocalCollection().getFirstSnReceived();
-                        
+
                         if (RecConn.getLocalCollection().read(n) == null || RecConn.getLocalCollection().read(n).length <= 0)
                         {
                             while (n >= RecConn.getLocalCollection().getFirstSnReceived() && (RecConn.getLocalCollection().read(n)== null || RecConn.getLocalCollection().read(n).length == 0))
@@ -304,7 +304,7 @@ public class RecoveryServerThread extends Thread
 
                             //##SE NON NE E' STATO TROVATO UNO PRIMA DI AVER RAGGIUNTO L'INIZIO DELLA COLLEZIONE, GENERA UN PACCHETTO VUOTO
                             if (n < RecConn.getLocalCollection().getFirstSnReceived())
-                            {    
+                            {
                                 n = i;
                                 byte[] emptyPkt = new byte[RecConn.getLocalCollection().getPktSize()];
                                 if (RecConn.getLocalCollection().debug)
@@ -313,25 +313,25 @@ public class RecoveryServerThread extends Thread
                                 {
                                     emptyPkt[s] = 0;
                                 }
-                                RecConn.getLocalCollection().recover(n, emptyPkt, false); //il pacchetto vuoto è "singolo" (lungo pktSize)
+                                RecConn.getLocalCollection().recover(n, emptyPkt, false); //il pacchetto vuoto Ã¨ "singolo" (lungo pktSize)
                             }
                         }
-                        
-                            
+
+
                        temp = RecConn.getLocalCollection().read(n);
                        if (RecConn.getLocalCollection().isMarked(n))
                        {
                            send[totaleByteRisposta] = separatoreMarked;
                            lunghezzaTemp = 2*pktSize;
                        }
-                       else 
+                       else
                        {
                            send[totaleByteRisposta] = separatoreNonMarked;
                            lunghezzaTemp = pktSize;
                        }
                        totaleByteRisposta += 1;
-                       
-                       //##AGGIUNGI IL CONTENUTO DEL PACCHETTO ALL'ARRAY DI BYTE DA INVIARE AL THREAD CLIENT DELL'INTERLOCUTORE CHE HA FORMULATO LA RICHIESTA 
+
+                       //##AGGIUNGI IL CONTENUTO DEL PACCHETTO ALL'ARRAY DI BYTE DA INVIARE AL THREAD CLIENT DELL'INTERLOCUTORE CHE HA FORMULATO LA RICHIESTA
 //                        if (temp != null)
 //                        {
                         try {
@@ -342,30 +342,30 @@ public class RecoveryServerThread extends Thread
                             if (RecConn.getLocalCollection().debug)
                                 System.out.println("pacchetto " + i + "non ancora inserito nella localCollection");
                         }
-                        
+
                         totalePkt++;
                         totaleByteRisposta += lunghezzaTemp;
 //                        }
-                      
-                      //##DEBUG  
+
+                      //##DEBUG
                       if (RecConn.getLocalCollection().debug && temp != null)
                         {
                         System.out.print("--SERVER-- Inviato Sn " + i + ": ");
-                        
+
                         for (int p = 0; p <= temp.length -1; p++)
                         {
                             System.out.print(temp[p] + " ");
                         }
-                        
+
                         System.out.println("");
                         }
-                       
+
                     }
-                    
+
                 }
-                
+
                 //##INVIO DELL'ARRAY SEND
-                if (!endServerThread) 
+                if (!endServerThread)
                 {
                     try {
                         if (RecConn.getClientSocket().isConnected())
@@ -391,11 +391,11 @@ public class RecoveryServerThread extends Thread
                         e.printStackTrace();
                     }
                 }
-                
+
             }
-            
+
         }//end while (!lengthRead || !queryRead)
-		
+
         //##CHIUSURA DEGLI STREAM TRA SERVER LOCALE E CLIENT REMOTO
         try {
             if (RecConn.getLocalCollection().debug)
@@ -406,12 +406,12 @@ public class RecoveryServerThread extends Thread
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        
+
         System.out.println("--RECOVERY-- RecoveryServerThread END");
-		
+
 	}
-	
-	
+
+
 	private static byte[] arrayResize(byte[] b, int newSize)
 	{
 	    byte[] newArray = new byte[newSize];
